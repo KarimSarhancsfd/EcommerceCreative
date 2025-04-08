73831c7b1a940d974f5b13e50fc8e3315f199f3f
unknown <kimokomono74@gmail.com>
2025-04-08 16:01:15 +0200

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Example from "./components/Header"
 
+import "./index.css"
 
 function App() {
 
   return (
    <div>
-        <h1>hello</h1>
+        {/* <Example /> */}
+        <section className="m-12 flex h-screen items-center justify-center  tw:bg-orange-5 p-12 text-center text-3xl md:p-14 lg:p-16 xl:p-20">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt accusamus molestiae neque, impedit dolores
+           ullam nesciunt voluptatum error porro! Voluptatibus ducimus error ad excepturi dolores
+            repudiandae perferendis natus sint doloribus.</p>
+            </section>
    </div>
   )
 }
